1dbf6f4587723c516235ea81c802709406306db1
r0ry64 <rorybeatz@gmail.com>
2024-12-07 00:56:32 -0500

    added homepage

---
+++ home.js
@@ -0,0 +1,4 @@
+function renderHome(container) {
+    container.innerHTML = `<h1>Welcome to the Home Page!!</h1>`;
+    container.innerHTML = `<p>This is the homepage, I dont know what else to put here lol.</p>`;
+}
